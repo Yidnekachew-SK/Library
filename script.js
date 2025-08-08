e3e55cc021688fd8d5566544086d7872d514679e
Yidnekachew S. <yidnekachewsk@gmail.com>
2025-08-08 17:17:21 +0300

    Refactor the code to use classes
    The code is refactored to use classes instead of constructor and functions.

--- script.js
+++ script.js
@@ -1,121 +1,137 @@
-const myLibrary = [];
-let container = document.querySelector(".book-container");
+class Library {
+	Library = [];
 
-function Book(title, author, pages, status){
-	if(!new.target){
-		console.log("can not call the function without instance of the object")
+	get Library(){
+		return this.Library;
 	}
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.status = status;
-}
 
-Book.prototype.changeStatus = function(){
-	if(this.status === "Read"){
-		this.status = "Not read";
-	}else{
-		this.status = "Read";
+	addBookToLibrary(bookObjects){
+		let id = crypto.randomUUID();
+		bookObjects.Id = id;
+		this.Library.push(bookObjects);
+
+		return this.Library;
 	}
 }
 
-function addBookToLibrary(bookObjects){
-	let id = crypto.randomUUID();
-	bookObjects.Id = id;
-	myLibrary.push(bookObjects);
 
-	return myLibrary;
-}
+class Book extends Library{
+	bookNo = 0;
+	container = document.querySelector(".book-container");
 
-let bookNo = 0;
-function displayBooks(books){
-	
-	let book = document.createElement("div");
-	book.className = "book-card";
-	book.setAttribute("id","book-" + myLibrary[bookNo].Id);
-	container.prepend(book);
+	constructor(title, author, pages, status){
+		super()
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.status = status;
+	}
 
-	let statusParagraph;
-	for(let j = 0; j < 5; j++){
-		let paragraph = document.createElement("p");
-		book.prepend(paragraph);
-		let para = document.querySelector("p");
-
-		switch(j){
-			case 0:
-				para.textContent = "Status: " + myLibrary[bookNo].status;
-				statusParagraph = para;
-				break;
-			case 1:
-				para.textContent = "No of pages: " + myLibrary[bookNo].pages;
-				break;
-			case 2:
-				para.textContent = "Author: " + myLibrary[bookNo].author;
-				break;
-			case 3:
-				para.textContent = "ID: " + myLibrary[bookNo].Id;
-				break;
-			case 4:
-				para.textContent = "Title: " + myLibrary[bookNo].title;
-				break;
+	changeBookStatus(){
+		if(this.status === "Read"){
+			this.status = "Not read";
+		}else{
+			this.status = "Read";
 		}
+		return this.status;
 	}
-	let buttonContainer = document.createElement("div");
-	buttonContainer.className = "button-container";
-	book.append(buttonContainer);
 
-	let button = document.createElement("button");
-	button.textContent = "Remove";
-	button.className = "remove-book";
-	button.setAttribute("id", "book-" + myLibrary[bookNo].Id);
-	buttonContainer.append(button);
+	displayBooks(books){
+		let book = document.createElement("div");
+		book.className = "book-card";
+		book.setAttribute("id","book-" + this.Library[this.bookNo].Id);
+		this.container.prepend(book);
 
-	button.addEventListener("click", () => {
-		let bookId = event.target.id;
-		let bookToRemove = document.querySelector(`#${bookId}`);
-		bookToRemove.parentElement.removeChild(bookToRemove);
-	})
+		let statusParagraph;
+		for(let j = 0; j < 5; j++){
+			let paragraph = document.createElement("p");
+			book.prepend(paragraph);
+			let para = document.querySelector("p");
 
-	let toggleButton = document.createElement("button");
-	toggleButton.textContent = "Change status";
-	toggleButton.className = "change-status"
-	buttonContainer.append(toggleButton);
+			switch(j){
+				case 0:
+					para.textContent = "Status: " + this.Library[this.bookNo].status;
+					statusParagraph = para;
+					break;
+				case 1:
+					para.textContent = "No of pages: " + this.Library[this.bookNo].pages;
+					break;
+				case 2:
+					para.textContent = "Author: " + this.Library[this.bookNo].author;
+					break;
+				case 3:
+					para.textContent = "ID: " + this.Library[this.bookNo].Id;
+					break;
+				case 4:
+					para.textContent = "Title: " + this.Library[this.bookNo].title;
+					break;
+			}
+		}
 
-	toggleButton.addEventListener("click", () => {
-		books.changeStatus();
-		statusParagraph.textContent = "Status: " + books.status;
-	})
 
-	bookNo++;
+		let buttonContainer = document.createElement("div");
+		buttonContainer.className = "button-container";
+		book.append(buttonContainer);
+
+		let button = document.createElement("button");
+		button.textContent = "Remove";
+		button.className = "remove-book";
+		button.setAttribute("id", "book-" + this.Library[this.bookNo].Id);
+		buttonContainer.append(button);
+
+		button.addEventListener("click", () => {
+			let bookId = event.target.id;
+			let bookToRemove = document.querySelector(`#${bookId}`);
+			bookToRemove.parentElement.removeChild(bookToRemove);
+		})
+
+		let toggleButton = document.createElement("button");
+		toggleButton.textContent = "Change status";
+		toggleButton.className = "change-status"
+		buttonContainer.append(toggleButton);
+
+		toggleButton.addEventListener("click", () => {
+			books.changeBookStatus();
+			statusParagraph.textContent = "Status: " + books.status;
+		})
+
+		this.bookNo++;
+		this.addBookButton();
+		this.getFormData();
+	}
+
+	addBookButton(){
+		const addButton = document.querySelector(".addBook");
+		addButton.addEventListener("click", () =>{
+			let form = document.querySelector("form");
+			form.style.display = "block";
+		})
+	}
+
+	getFormData(){
+		const form = document.querySelector("#book-form");
+		form.addEventListener("submit", (event) => {
+			event.preventDefault();
+
+			const formData = new FormData(event.target);
+
+			let title = formData.get('Title');
+			let author = formData.get('Author');
+			let pages = formData.get('Pages');
+			let read = formData.get('Status');
+
+			let addedBook = new Book(title, author, pages, read);
+			this.addBookToLibrary(addedBook);
+			this.displayBooks(addedBook);
+
+			let bookForm = document.querySelector("form");
+			bookForm.style.display = "none";
+			form.reset();
+		})
+	}
 }
-
-const addButton = document.querySelector(".addBook");
-addButton.addEventListener("click", () =>{
-	let form = document.querySelector("form");
-	form.style.display = "block";
-})
-
-const form = document.querySelector("#book-form");
-form.addEventListener("submit", (event) => {
-	event.preventDefault();
-
-	const formData = new FormData(event.target);
-
-	let title = formData.get('Title');
-	let author = formData.get('Author');
-	let pages = formData.get('Pages');
-	let read = formData.get('Status');
-
-	let addedBook = new Book(title, author, pages, read);
-	addBookToLibrary(addedBook);
-	displayBooks(addedBook);
-
-	let bookForm = document.querySelector("form");
-	bookForm.style.display = "none";
-	form.reset();
-})
 
 const book1 = new Book("Book1", "someone", 100, "Not read");
 
-addBookToLibrary(book1);
-displayBooks(book1);
+book1.addBookToLibrary(book1);
+book1.displayBooks(book1);
